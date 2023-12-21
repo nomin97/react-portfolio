0f4ddf268c830cf659c5f05391877f1a9923d20f
Nomin Myagmarsuren <nominmyagmarsuren@gmail.com>
2023-12-20 20:38:14 -0700

    vite config files

--- src/main.jsx
+++ src/main.jsx
@@ -1,42 +1,10 @@
-import ReactDOM from 'react-dom/client';
-// Bringing in the required imports from 'react-router-dom' to set up application routing behavior
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
-
-// Bringing in the pages the router will use to conditionally show the appropriate views
-import App from './app';
-import ErrorPage from './pages/ErrorPage';
-import HomePage from './pages/HomePage';
-import ProfilePage from './pages/ProfilePage';
-import AboutPage from './pages/AboutPage';
-import ProjectsPage from './pages/Projects';
-
-// Define the accessible routes, and which components respond to which URL
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        index: true,
-        element: <HomePage />,
-      },
-      {
-        path: 'profile/:id',
-        element: <ProfilePage />,
-      },
-      {
-        path: 'about',
-        element: <AboutPage />,
-      },
-    ],
-  },
-]);
-
-// Render the RouterProvider component
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
-);
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
