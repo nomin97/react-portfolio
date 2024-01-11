a7725087cd105aa76e34753e6a64384ea614e26b
Nomin Myagmarsuren <nominmyagmarsuren@gmail.com>
2024-01-11 11:25:24 -0700

    footer links

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,15 +4,11 @@ function Footer() {
     const icons = [
       {
         name: "fab fa-github",
-        link: "https://github.com/"
+        link: "https://github.com/nomin97"
       },
       {
         name: "fab fa-linkedin",
-        link: "https://www.linkedin.com/"
-      },
-      {
-        name: "fab fa-stack-overflow",
-        link: "https://stackoverflow.com/"
+        link: "https://www.linkedin.com/in/nominmyag"
       }
     ]
   
