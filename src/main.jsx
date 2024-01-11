50588b74c8ced956cf8f9cc42b30065c8907f68e
Nomin Myagmarsuren <nominmyagmarsuren@gmail.com>
2024-01-10 23:07:51 -0700

    updated headers

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx';
-import Error from './components/ErrorPage';
-import About from './components/About';
-import Portfolio from './components/Portfolio';
-import Contact from './components/Contact';
-import Resume from './components/Resume';
+import Error from './components/ErrorPage/index.jsx';
+import About from './components/About/index.jsx';
+import Portfolio from './components/Portfolio/index.jsx';
+import Contact from './components/Contact/index.jsx';
+import Resume from './components/Resume/index.jsx';
 import './index.css';
 
 const router = createBrowserRouter([
